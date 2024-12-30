c24102d6b26d2898d66a5284f60fda414c829c96
yudhaeric <yudhaericpamungkas@gmail.com>
2024-12-31 02:14:32 +0700

    Update Migration File

--- database/migrations/2024_12_30_191320_create_media_galleries_table.php
+++ database/migrations/2024_12_30_191320_create_media_galleries_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('media_gallery', function (Blueprint $table) {
+        Schema::create('galleries', function (Blueprint $table) {
             $table->id();
             $table->foreignId('customer_id')->constrained()->onDelete('cascade');
             $table->string('file_path');
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('media_gallery');
+        Schema::dropIfExists('media_galleries');
     }
 };
